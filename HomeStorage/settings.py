b36c7e438d68cff137ff01ade74514a5ae63d30a
Rohan Paramanand <rohanparamanand@gmail.com>
2025-06-18 12:13:21 +0530

    Static files

--- HomeStorage/settings.py
+++ HomeStorage/settings.py
@@ -112,7 +112,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+# Add this in later when using templates
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
